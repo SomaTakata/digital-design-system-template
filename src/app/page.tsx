91006c4ecb1d8548f179016bf08c6f1b5359c6ee
SomaTakata <somatakata@gmail.com>
2025-04-20 20:39:01 +0900

    change ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components";
+
 export default function Page() {
-  return <div className="h-[800px] w-[393px]"></div>;
+  return (
+    <div className="h-[800px] w-[393px] flex justify-center items-center">
+      <Button size="md">Hello</Button>
+    </div>
+  );
 }
